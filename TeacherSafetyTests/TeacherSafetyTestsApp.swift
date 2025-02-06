53d3d9b90a3efffbf7a8973f8f1699d1750b6d9c
MaxDroll <mdroll6088@stu.d214.org>
2025-02-06 07:52:54 -0800

    Initial Commit

---
+++ TeacherSafetyTests/TeacherSafetyTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeacherSafetyTestsApp.swift
+//  TeacherSafetyTests
+//
+//  Created by Maxwell K. Droll on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct TeacherSafetyTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
