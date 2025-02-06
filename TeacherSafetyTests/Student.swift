e57fd9fdcca4ca7355544640a9c653645b514ccc
MaxDroll <mdroll6088@stu.d214.org>
2025-02-06 08:17:27 -0800

    added firebase

---
+++ TeacherSafetyTests/Student.swift
@@ -0,0 +1,22 @@
+//
+//  Student.swift
+//  TeacherSafetyTests
+//
+//  Created by Maxwell K. Droll on 2/6/25.
+//
+
+import SwiftUI
+
+struct Student: Decodable{
+    var AngleGrinderTest: Int
+    var AngleGrinderVideo: Bool
+    var Class: String
+    var LatheTest: Int
+    var LatheVideo: Bool
+    var MillTest: Int
+    var MillVideo: Bool
+    var WelderTest: Int
+    var WelderVideo: Bool
+    var name: String
+    var Teacher: Bool
+}
