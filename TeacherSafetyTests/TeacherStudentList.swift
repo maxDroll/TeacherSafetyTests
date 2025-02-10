61cb3337be01043da98a91562f4fb4f3a3ed32c6
MaxDroll <mdroll6088@stu.d214.org>
2025-02-10 09:10:01 -0800

    Sorted the students by class

--- TeacherSafetyTests/TeacherStudentList.swift
+++ TeacherSafetyTests/TeacherStudentList.swift
@@ -12,32 +12,41 @@ import FirebaseFirestore
 struct StudentList: View {
     @FirestoreQuery(collectionPath: "Students") var students:[Student]
     @Binding var selectedStudent: Student
+    @State var selectedClass = ""
+    @State var updatedClass = ""
     var body: some View {
         ScrollView(.vertical){
+            TextField("Enter Class", text: $selectedClass)
+                .offset(x: 15)
+                .onSubmit {
+                    updatedClass = selectedClass
+                }
             VStack(alignment: .leading, spacing:0){
                 Rectangle()
                     .foregroundStyle(.black)
                     .frame(width: 500, height: 5)
                 ForEach(students, id: \.name){student in
-                    Button {
-                        selectedStudent = student
-                    } label: {
-                        ZStack{
-                            Text(student.name)
-                                .frame(width: 450, height: 100, alignment: .leading)
-                                .font(.system(size: 30))
-                            if selectedStudent.name == student.name && selectedStudent.Class == student.Class{
-                                Rectangle()
-                                    .foregroundStyle(.blue)
-                                    .opacity(0.3)
-                                    .frame(width: 500, height: 100)
+                    if student.Class == updatedClass{
+                        Button {
+                            selectedStudent = student
+                        } label: {
+                            ZStack{
+                                Text(student.name)
+                                    .frame(width: 450, height: 100, alignment: .leading)
+                                    .font(.system(size: 30))
+                                if selectedStudent.name == student.name && selectedStudent.Class == student.Class{
+                                    Rectangle()
+                                        .foregroundStyle(.blue)
+                                        .opacity(0.3)
+                                        .frame(width: 500, height: 100)
+                                }
                             }
+                            .frame(width: 500)
                         }
-                        .frame(width: 500)
+                        Rectangle()
+                            .foregroundStyle(.black)
+                            .frame(width: 500, height: 5)
                     }
-                    Rectangle()
-                        .foregroundStyle(.black)
-                        .frame(width: 500, height: 5)
                 }
             }
             .frame(width: 500)
