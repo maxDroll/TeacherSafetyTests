fd47d7e40b06fc2326ebb8c0db467e65293070ae
Eric Wetzel <ewetzel5400@stu.d214.org>
2025-02-10 10:48:38 -0600

    Added Navigation Stack

--- TeacherSafetyTests/ContentView.swift
+++ TeacherSafetyTests/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TeacherStudentSelectionOrganizer()
+        NavigationStack{
+            NavigationLink(destination: TeacherStudentSelectionOrganizer()) {
+                
+            }
+        }
     }
-}
-
-#Preview {
-    ContentView()
+    
 }
