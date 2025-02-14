6a76dc580abf7e6f6f74b1c93da1a06ee0dd8c5a
MaxDroll <mdroll6088@stu.d214.org>
2025-02-14 08:17:55 -0800

    worked on aesthetics of the Student Stats view

--- TeacherSafetyTests/StudentStats.swift
+++ TeacherSafetyTests/StudentStats.swift
@@ -14,28 +14,51 @@ struct StudentStats: View {
     let machines = ["Lathe", "Mille", "Welder", "Angle Grinder"]
     var body: some View {
         VStack{
-            HStack{
-                Text("Lathe")
-                Text("\(selectedStudent.LatheTest)")
-                Text("\(selectedStudent.LatheVideo)")
-            }
-            HStack{
-                Text("Mille")
-                Text("\(selectedStudent.MillTest)")
-                Text("\(selectedStudent.MillVideo)")
-            }
-            HStack{
-                Text("Welder")
-                Text("\(selectedStudent.WelderTest)")
-                Text("\(selectedStudent.WelderVideo)")
-            }
-            HStack{
-                Text("Angle Grinder")
-                Text("\(selectedStudent.AngleGrinderTest)")
-                Text("\(selectedStudent.AngleGrinderVideo)")
-            }
-
+            Rectangle()
+                .foregroundStyle(.black)
+                .frame(width: 700, height: 5)
+            machine(machineVideo: selectedStudent.LatheVideo, machineTest: selectedStudent.LatheTest, machineName: "Lathe")
+            machine(machineVideo: selectedStudent.MillVideo, machineTest: selectedStudent.MillTest, machineName: "Mill")
+            machine(machineVideo: selectedStudent.WelderVideo, machineTest: selectedStudent.WelderTest, machineName: "Welder")
+            machine(machineVideo: selectedStudent.AngleGrinderVideo, machineTest: selectedStudent.AngleGrinderTest, machineName: "Angle Grinder")
         }
         .frame(width: 700)
+    }
+    struct machine: View {
+        let machineVideo: Bool
+        let machineTest: Int
+        let machineName: String
+        var body: some View {
+            Text(machineName)
+                .font(.largeTitle)
+            HStack{
+                VStack(spacing: 20){
+                    Text("Video Status")
+                        .font(.title)
+                    Text("Test Score")
+                        .font(.title)
+                }
+                .frame(width: 200)
+                VStack(spacing: 20){
+                    if machineVideo{
+                        Text("Watched")
+                            .font(.title)
+                    }else{
+                        Text("Not Watched")
+                            .font(.title)
+                    }
+                    if machineTest == -1{
+                        Text("Not Taken")
+                            .font(.title)
+                    }else{
+                        Text("\(machineTest)/5")
+                            .font(.title)
+                    }
+                }
+            }
+            Rectangle()
+                .foregroundStyle(.black)
+                .frame(width: 700, height: 5)
+        }
     }
 }
